9b2fa947da598f2cf24552145a1b03a1e90e4aba
Julian <Julian@192.168.0.4>
2021-09-13 16:20:40 -0500

    version 2

---
+++ Desktop/actividad1/Practica2/src/main/java/Ejercicio1.java
@@ -0,0 +1,16 @@
+
+public class Practica0 {
+
+   public static void main(String[] args) {
+        int N = 5;
+        double A = 4.56;
+        char C = 'a';
+        System.out.println("Variable N = " + N);
+        System.out.println("Variable A =" + A);
+        System.out.println("Variable C = " + C);
+        System.out.println(N + "+" + A + " = " + (N+A));
+        System.out.println(A + "-" + N +" = " + (A-N));
+        System.out.println("Valor numerico del caracter" + C + "=" + (int)C); 
+    }
+    
+}
