4abac743396e5c07ddab2c62d057e6fa4e6418ea
Julian <Julian@192.168.0.4>
2021-09-13 19:42:39 -0500

    version 3

---
+++ Desktop/actividad1/Ejercicios3/src/main/java/Ejercicio4.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio4 {
+  
+       public static void main(String[] args) {
+           long numero1 = 8;
+           int numero2 = 13;
+           long resta = numero1-numero2;
+           System.out.println("El resultado es "+resta);
+ }
+  
+}
---
+++ Desktop/actividad1/Ejercicios3/src/main/java/Ejercicio5.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio5 {
+
+     public static void main(String[] args) {
+       int numero1 = 52;
+       int numero2 = 2;
+       int suma = numero1+numero2;
+       System.out.println("El resultado es "+suma);
+    }
+    
+}
