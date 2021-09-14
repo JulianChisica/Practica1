1958a90bbdf8d39364967bdd718f2d5a114dc9ac
Julian <Julian@192.168.0.4>
2021-09-13 19:36:26 -0500

    version 2

---
+++ Desktop/actividad1/Ejercicios3/src/main/java/Ejercicios6.java
@@ -0,0 +1,13 @@
+
+public class Ejercicios6 {
+
+  
+    public static void main(String[] args) {
+            double numero1 = 7.6;
+            int numero2 = 4;
+            double multiplicacion = numero1*numero2;
+            System.out.println("El resultado es "+multiplicacion);
+    
+ }
+    
+}
